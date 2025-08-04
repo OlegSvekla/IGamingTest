3aea9f5fef28b845f1e74e860a1b328ecb336bb5
OlegSvekla <olegsvekla993@gmail.com>
2025-08-04 11:27:53 +0300

    fix controllers spelling

--- IGamingTest/IGamingTest.Web/Controllers/IGamingTestController.cs
+++ IGamingTest/IGamingTest.Web/Controllers/IGamingTestController.cs
@@ -9,35 +9,34 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 using static Microsoft.AspNetCore.Http.StatusCodes;
 
-namespace IGamingTest.Web.Controllers
+namespace IGamingTest.Web.Controllers;
+
+[ApiController]
+[ApiVersion(1)]
+[Route("api/v{version:apiVersion}/[controller]")]
+[Consumes(MediaTypeNames.Application.Json)]
+[Produces(MediaTypeNames.Application.Json)]
+[ProducesResponseType(Status400BadRequest, Type = typeof(FieldErrorRs[]))]
+[ProducesResponseType(Status500InternalServerError, Type = typeof(MessageErrorRs))]
+[ProducesResponseType(Status429TooManyRequests, Type = typeof(void))]
+public class IGamingTestController(
+    ILocalMessageBus localMessageBus,
+    IMapper<GetMeteoriteFilterRq, GetMeteoriteFilterQuery> meteoriteFilterRqToQueryMapper,
+    IMapper<IReadOnlyCollection<GetMeteoriteFilterQueryRs>, IReadOnlyCollection<GetMeteoriteFilterRs>> meteoriteFilterQueryRsToRsMapper)
+    : ControllerBase
 {
-    [ApiController]
-    [ApiVersion(1)]
-    [Route("api/v{version:apiVersion}/[controller]")]
-    [Consumes(MediaTypeNames.Application.Json)]
-    [Produces(MediaTypeNames.Application.Json)]
-    [ProducesResponseType(Status400BadRequest, Type = typeof(FieldErrorRs[]))]
-    [ProducesResponseType(Status500InternalServerError, Type = typeof(MessageErrorRs))]
-    [ProducesResponseType(Status429TooManyRequests, Type = typeof(void))]
-    public class IGamingTestController(
-        ILocalMessageBus localMessageBus,
-        IMapper<GetMeteoriteFilterRq, GetMeteoriteFilterQuery> meteoriteFilterRqToQueryMapper,
-        IMapper<IReadOnlyCollection<GetMeteoriteFilterQueryRs>, IReadOnlyCollection<GetMeteoriteFilterRs>> meteoriteFilterQueryRsToRsMapper)
-        : ControllerBase
+    [HttpPost("meteorites")]
+    [ProducesResponseType(Status200OK, Type = typeof(IReadOnlyCollection<GetMeteoriteFilterRs>))]
+    public async Task<IActionResult> GetEventsAsync(
+        [FromBody] GetMeteoriteFilterRq rq,
+        CancellationToken ct)
     {
-        [HttpPost("meteorites")]
-        [ProducesResponseType(Status200OK, Type = typeof(IReadOnlyCollection<GetMeteoriteFilterRs>))]
-        public async Task<IActionResult> GetEventsAsync(
-            [FromBody] GetMeteoriteFilterRq rq,
-            CancellationToken ct)
-        {
-            var query = meteoriteFilterRqToQueryMapper.Map(rq);
+        var query = meteoriteFilterRqToQueryMapper.Map(rq);
 
-            var queryRs = await localMessageBus.DispatchAsync(query, ct);
+        var queryRs = await localMessageBus.DispatchAsync(query, ct);
 
-            var rs = meteoriteFilterQueryRsToRsMapper.Map(queryRs);
+        var rs = meteoriteFilterQueryRsToRsMapper.Map(queryRs);
 
-            return Ok(rs);
-        }
+        return Ok(rs);
     }
 }
